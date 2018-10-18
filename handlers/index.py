0486e5877e3d6d330f26dbab2dd8d4f5f9b89cb5
tornado <tornado@ubuntu-server.mshome.net>
2018-10-18 22:48:56 +0800

    fixed whend system init but index status error

--- handlers/index.py
+++ handlers/index.py
@@ -5,7 +5,7 @@ from handlers.base import BaseHandler
 from methods.controller import PageController
 from orm.issues_info import IssuesInfoModule
 from orm.meeting_info import MeetingInfoModule
-
+from methods.debug import *
 
 # 继承 base.py 中的类 BaseHandler
 class IndexHandler(BaseHandler):
@@ -17,10 +17,16 @@ class IndexHandler(BaseHandler):
         render_controller = page_controller.get_render_controller()
 
         render_controller["index"] = True
-        render_controller["authorized"] = self.session["authorized"]
         render_controller["login"] = False
-        render_controller["admin"] = self.session["admin"]
-        render_controller["organizer"] = self.session["organizer"]
+
+        if self.session["authorized"] is None:
+            render_controller["authorized"] = False
+            render_controller["admin"] = False
+            render_controller["organizer"] = False
+        else:
+            render_controller["authorized"] = self.session["authorized"]
+            render_controller["admin"] = self.session["admin"]
+            render_controller["organizer"] = self.session["organizer"]
 
         user_name = self.get_current_user()
         topics_table = self.__get_all_topic_tables()
