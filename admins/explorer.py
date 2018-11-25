b6e8c3cb77d4f55daadb98eda237537b276581d5
tornado <tornado@ubuntu-server.mshome.net>
2018-11-25 12:37:04 +0800

    add score file download

--- admins/explorer.py
+++ admins/explorer.py
@@ -8,6 +8,7 @@ from admins.decorator import admin_post_auth
 from methods.debug import *
 from openpyxl import Workbook
 from orm.users_info import UsersInfoModule
+from orm.score_info import ScoreInfoModule
 
 
 # 继承 base.py 中的类 BaseHandler
@@ -38,6 +39,15 @@ class FileDownLoadHandler(BaseHandler):
         logging.info("file name is:"+filename)
         if table_name == "member_table":
             self.__write_user_info_to_xlsx(filename)
+        elif table_name == "points_table":
+            self.__write_score_info_to_xlsx(filename)
+        else:
+
+            self.render("admin/file_error.html",
+                        controller=self.render_controller,
+                        language_mapping=self.language_mapping,
+                        )
+            return
 
         self.set_header('Content-Type', 'application/octet-stream')
         self.set_header('Content-Disposition', ('attachment; filename=%s' % filename).encode('utf-8'))
@@ -75,16 +85,48 @@ class FileDownLoadHandler(BaseHandler):
         #  2.存具体信息
         index = 2
         for user in user_tables:
-            booksheet.cell(index, 1).value = user.id  # "编号"
-            booksheet.cell(index, 2).value = user.user_name  # "用户名"
-            booksheet.cell(index, 3).value = user.chinese_name  # "中文名"
-            booksheet.cell(index, 4).value = user.nick_name  # "昵称"
-            booksheet.cell(index, 5).value = user.pass_word  # "密码"
-            booksheet.cell(index, 6).value = user.email  # "邮箱"
-            booksheet.cell(index, 7).value = user.department  # "部门"
-            booksheet.cell(index, 8).value = user.user_role  # "角色"
-            booksheet.cell(index, 9).value = user.address  # "地址"
-            booksheet.cell(index, 10).value = user.change_pwd_count  # "修改密码次数"
+            if user.user_role != "root":
+                booksheet.cell(index, 1).value = user.id  # "编号"
+                booksheet.cell(index, 2).value = user.user_name  # "用户名"
+                booksheet.cell(index, 3).value = user.chinese_name  # "中文名"
+                booksheet.cell(index, 4).value = user.nick_name  # "昵称"
+                if user.pwd_modified is True:
+                    booksheet.cell(index, 5).value = "******"  # "密码"
+                else:
+                    booksheet.cell(index, 5).value = user.pass_word  # "密码"
+
+                booksheet.cell(index, 6).value = user.email  # "邮箱"
+                booksheet.cell(index, 7).value = user.department  # "部门"
+                booksheet.cell(index, 8).value = user.user_role  # "角色"
+                booksheet.cell(index, 9).value = user.address  # "地址"
+                booksheet.cell(index, 10).value = user.change_pwd_count  # "修改密码次数"
+
+                index = index + 1
+
+        workbook.save(file_name)
+
+    def __write_score_info_to_xlsx(self, file_name):
+
+        workbook = Workbook()
+        booksheet = workbook.active  # 获取当前活跃的sheet,默认是第一个sheet
+        # 存第一行单元格cell(1,1)
+        # 1、存标题
+        booksheet.cell(1, 1).value = "编号"  # 这个方法索引从1开始
+        booksheet.cell(1, 2).value = "用户名"
+        booksheet.cell(1, 3).value = "中文名"
+        booksheet.cell(1, 4).value = "当前得分"
+        booksheet.cell(1, 5).value = "上次得分"
+
+        score_tables = self.db.query(ScoreInfoModule).all()
+
+        #  2.存具体信息
+        index = 2
+        for score in score_tables:
+            booksheet.cell(index, 1).value = score.id  # "编号"
+            booksheet.cell(index, 2).value = score.user_name  # "用户名"
+            booksheet.cell(index, 3).value = score.chinese_name  # "中文名"
+            booksheet.cell(index, 4).value = score.current_scores  # "当前得分"
+            booksheet.cell(index, 5).value = score.last_scores  # "上次得分"
 
             index = index + 1
         workbook.save(file_name)
